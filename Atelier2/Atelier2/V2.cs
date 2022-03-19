e6ecaa47ef3f1245c3aeef961b24449020d42ee2
salmakorchi <57690392+salmakorchi@users.noreply.github.com>
2022-03-19 12:00:55 +0100

    third Commit

---
+++ Atelier2/Atelier2/V2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Atelier2
+{
+    public partial class V2 : Form
+    {
+        public V2()
+        {
+            InitializeComponent();
+        }
+    }
+}
